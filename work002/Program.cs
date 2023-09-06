98752935516f66a53a1e6bf93c8f417c11b9ba72
MarkusLinV <maksim.lin.00@mail.ru>
2023-09-06 09:35:24 +0200

    готовое сравнение 2-х чисел

--- work002/Program.cs
+++ work002/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+using System.Diagnostics;
+
+Console.Write("Введите число №1: ");
+int nam1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int nam2 = Convert.ToInt32(Console.ReadLine());
+if(nam1 > nam2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(nam1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(nam2);
+}
